5bff6ec55bdec1f8476aa74b371efb3366f5e6fb
EmersonBarranco <126826866+Dakkarh@users.noreply.github.com>
2024-12-03 15:35:55 -0500

    api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,9 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 
-@Module({
-  providers: [UsersService],
-  controllers: [UsersController]
-})
+@Module({})
 export class UsersModule {}
